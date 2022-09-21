9568052f3360df369b262fc72623abf626268401
hoa2000kxpt <hoa2000kxpt@gmail.com>
2022-09-21 23:04:59 +0700

    initial version

---
+++ why-name.py
@@ -0,0 +1,24 @@
+# class User {
+#   save() {}
+# }
+
+# const isLoggedIn = true;
+
+# const user = new User();
+# user.save();
+
+# if (isLoggedIn) {
+#   // ...
+# }
+
+class User:
+  save()
+
+is_logged_in = True
+
+user = User()
+user.save()
+
+if (is_logged_in):
+  #...
+  print("Nice!")
